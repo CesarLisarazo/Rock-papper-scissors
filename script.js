ecb5de59f288c0c0d4321ffc87a7206fa3a3d3a8
CesarLisarazo <cesarlisarazo@gmail.com>
2024-09-12 21:50:10 -0300

    style

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let userChoiceDisplay = document.getElementById('userChoice');
 let resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('.button');
 let reload = document.getElementById("reload")
-reload.addEventListener ("click", reloadPage)
+reload.addEventListener("click", reloadPage)
 let userChoice;
 let computerChoice;
 let result;
-let face= document.getElementById('face')
-let bottomWin=document.getElementById("win")
-let bottomLoose=document.getElementById("loose")
-let bottomDraw=document.getElementById("draw")
+let face = document.getElementById('face')
+let bottomWin = document.getElementById("win")
+let bottomLoose = document.getElementById("loose")
+let bottomDraw = document.getElementById("draw")
 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
@@ -39,16 +39,16 @@ function generateComputerChoice() {
 }
 
 function getResult() {
-     if (computerChoice == userChoice) {
+    if (computerChoice == userChoice) {
         result = "Draw!"
-     }
-
-   else if (computerChoice == "Rock" && userChoice == "Paper" || computerChoice == "Paper" && userChoice == "Scissors" || computerChoice == "Scissors" && userChoice == "Rock") {
-        result = "You win!"
-        
-     
     }
-   
+
+    else if (computerChoice == "Rock" && userChoice == "Paper" || computerChoice == "Paper" && userChoice == "Scissors" || computerChoice == "Scissors" && userChoice == "Rock") {
+        result = "You win!"
+
+
+    }
+
 
 
     else {
@@ -56,28 +56,36 @@ function getResult() {
     }
 
     resultDisplay.innerHTML = result
-    if( result=="Draw!"){
-       face.setAttribute("src","images/draw.png")
-       bottomDraw.innerHTML=Number(bottomDraw.innerHTML)+1
+
+    if (result == "Draw!") {
+
+        face.setAttribute("src", "images/draw.png")
+
+        resultDisplay.style.color = "#facd94"
+        bottomDraw.innerHTML = Number(bottomDraw.innerHTML) + 1
+
     }
 
-    else if(result=="You win!"){
-        face.setAttribute("src","images/win.png")
-        bottomWin.innerHTML=Number(bottomWin.innerHTML)+1
+    else if (result == "You win!") {
+        face.setAttribute("src", "images/win.png")
+        resultDisplay.style.color = "#8dc0d0"
+        bottomWin.innerHTML = Number(bottomWin.innerHTML) + 1
     }
-    else if(result=="You loose!"){
-        face.setAttribute("src","images/loose.png")
-        bottomLoose.innerHTML=Number(bottomLoose.innerHTML)+1
+    else if (result == "You loose!") {
+        face.setAttribute("src", "images/loose.png")
+        bottomLoose.innerHTML = Number(bottomLoose.innerHTML) + 1
+        resultDisplay.style.color = "#f484c1"
+        result.classList.add("perdiste")
     }
-   
 
 
-    
+
+
 }
 
 
-function reloadPage () {
-   location.reload()
+function reloadPage() {
+    location.reload()
 }
 
 
